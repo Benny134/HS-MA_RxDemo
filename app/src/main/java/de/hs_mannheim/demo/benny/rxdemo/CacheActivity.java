db4b07bd536bf216fc6472ff8e75df48977ae750
Benny <benni1304@gmail.com>
2016-04-21 20:08:03 +0200

    ...

--- app/src/main/java/de/hs_mannheim/demo/benny/rxdemo/CacheActivity.java
+++ app/src/main/java/de/hs_mannheim/demo/benny/rxdemo/CacheActivity.java
@@ -2,12 +2,70 @@ package de.hs_mannheim.demo.benny.rxdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
+
+import rx.Observable;
+import rx.Observer;
+import rx.Subscriber;
+import rx.Subscription;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.android.widget.OnTextChangeEvent;
+import rx.android.widget.WidgetObservable;
+import rx.functions.Action1;
+import rx.schedulers.TimeInterval;
 
 public class CacheActivity extends AppCompatActivity {
+    private ArrayList<Long> data = new ArrayList<>();
+    private ArrayList<Long> cache = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cache);
+
+        initSuggestionsView();
+        initObserver();
+        refresh();
+    }
+
+    private void initSuggestionsView(){
+        ListView autoCompletionSuggestions = (ListView) findViewById(R.id.lstCachingItems);
+
+        autoCompletionSuggestions.setAdapter(new ArrayAdapter<Long>(this, android.R.layout.simple_list_item_1, android.R.id.text1, data));
+    }
+
+    private void initObserver(){
+        for(int i = 0; i < 100; i++){
+            cache.add((long)(Math.random() * 100));
+        }
+
+        Observable<Long> cachingObserver = Observable.from(data);
+    }
+
+    public void onClickRefresh(View view){
+        refresh();
+    }
+
+    public void refresh(){
+        ToggleButton toggleButton = (ToggleButton) findViewById(R.id.tglWithCaching);
+        data.clear();
+        if(toggleButton.isChecked()){
+            data.addAll(cache);
+        }else{
+            for(int i = 0; i < 100; i++){
+                data.add((long)(Math.random() * 100));
+            }
+        }
+
+        ((ArrayAdapter<String>)((ListView)findViewById(R.id.lstCachingItems)).getAdapter()).notifyDataSetChanged();
     }
 }
--- app/src/main/java/de/hs_mannheim/demo/benny/rxdemo/MainActivity.java
+++ app/src/main/java/de/hs_mannheim/demo/benny/rxdemo/MainActivity.java
@@ -4,21 +4,9 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.EditText;
-import android.widget.ListView;
-import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
-
-import rx.Observable;
-import rx.android.schedulers.AndroidSchedulers;
-import rx.android.widget.OnTextChangeEvent;
-import rx.android.widget.WidgetObservable;
-import rx.functions.Action1;
 
 public class MainActivity extends AppCompatActivity {
     private List<String> fruits = new ArrayList<>();
